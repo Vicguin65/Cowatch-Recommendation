3da846efc827829bb07830135de38ec8729a7f76
Mohammed Raamiz Abbasi <raamizabbasi@gmail.com>
2024-06-05 20:58:07 -0400

    Added google sign in button

--- recommendation-website/src/App.js
+++ recommendation-website/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import GoogleButton from "react-google-button";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cowatch Recommendation</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <GoogleButton
+          onClick={() => {
+            console.log("Google button clicked");
+          }}
+        />
+      </div>
     </div>
   );
 }
