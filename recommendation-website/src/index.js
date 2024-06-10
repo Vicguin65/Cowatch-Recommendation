2d5a3f274c2979ba92c137862c4f630eff91c687
Tyler Du <35591950+Vicguin65@users.noreply.github.com>
2024-06-10 10:31:37 -0400

    Simple OAuth2

--- recommendation-website/src/index.js
+++ recommendation-website/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// TODO:
+// IMPLEMENT SECRETS
+const clientId = "ENTER IT HERE";
+
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={clientId}>
+        <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
