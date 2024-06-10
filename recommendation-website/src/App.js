581b10071572a9df6c95b302d35c69bd230dea7f
Mohammed Raamiz Abbasi <raamizabbasi@gmail.com>
2024-06-10 18:21:09 -0400

    Created Login Page

--- recommendation-website/src/App.js
+++ recommendation-website/src/App.js
@@ -1,35 +1,54 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { GoogleLogin } from "@react-oauth/google";
 
 import { jwtDecode } from "jwt-decode";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
+  const backgroundImageUrl =
+    "https://images.pexels.com/photos/1629236/pexels-photo-1629236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+
   const [jsonResponse, setJsonResponse] = useState(null);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Google login integration</p>
-        <span>
+    <div
+      style={{
+        backgroundImage: `url(${backgroundImageUrl})`,
+      }}
+      className="items-center flex flex-col bg-cover bg-center bg-no-repeat h-screen w-screen p-5"
+    >
+      <h1 className="text-5xl text-white font-bold mt-14">
+        Welcome to Cowatch!
+      </h1>
+      <h3 className="mt-5 text-2xl text-white font-light italic">
+        "Stream Together, Bond Forever"
+      </h3>
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <div className="py-8 px-12 flex flex-col justify-center items-center bg-slate-900 rounded-3xl shadow">
+          <h3 className="text-xl text-white mb-5">
+            Connect Your Google Account
+          </h3>
           <GoogleLogin
-            onSuccess={credentialResponse => {
+            onSuccess={(credentialResponse) => {
               console.log(credentialResponse);
               const decoded = jwtDecode(credentialResponse?.credential);
               setJsonResponse(decoded);
-              
             }}
             onError={() => {
-              console.log('Login Failed');
+              console.log("Login Failed");
             }}
           />
-          {jsonResponse && <pre>{"You are signed in as," + JSON.stringify(jsonResponse, null, 2)}</pre>
+          {/* {
+            jsonResponse && (
+              <pre>
+                {"You are signed in as," +
+                  JSON.stringify(jsonResponse, null, 2)}
+              </pre>
+            )
             // jsonResponse && <pre>{"You are signed in as," + JSON.stringify(jsonResponse.name, null, 2)}</pre>
-          }
-        </span>
-      </header>
+          } */}
+        </div>
+      </div>
     </div>
   );
 }
--- recommendation-website/src/index.js
+++ recommendation-website/src/index.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-// TODO:
-// IMPLEMENT SECRETS
-const clientId = "ENTER IT HERE";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
-        <App />
+      <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
