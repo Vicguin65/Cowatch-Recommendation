8faae2be0533588bd0942bb026dd0082b2496c66
Jaswanth Duddu <57510269+jaswanthDuddu@users.noreply.github.com>
2024-07-29 11:33:35 -0400

    Update serverDB.js
    Linted this file with standard JavaScript format

--- backend/serverDB.js
+++ backend/serverDB.js
@@ -1,31 +1,31 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const { OAuth2Client } = require("google-auth-library");
-const sqlite3 = require("sqlite3").verbose();
-const cors = require("cors");
-const { google } = require("googleapis");
-const youtube = google.youtube("v3");
-const jwt = require("jsonwebtoken");
+const express = require('express')
+const bodyParser = require('body-parser')
+const { OAuth2Client } = require('google-auth-library')
+const sqlite3 = require('sqlite3').verbose()
+const cors = require('cors')
+const { google } = require('googleapis')
+const youtube = google.youtube('v3')
+const jwt = require('jsonwebtoken')
 
-const app = express();
-const port = 5000;
+const app = express()
+const port = 5000
 
 // Fetch access and refresh tokens from Google
 const oauth2Client = new OAuth2Client(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  "http://localhost:5000/auth/callback" // Replace with your callback URL
-);
+  'http://localhost:5000/auth/callback' // Replace with your callback URL
+)
 
 // Middleware
-app.use(cors());
+app.use(cors())
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 // SQLite setup
-const db = new sqlite3.Database("./room-tracker.db");
+const db = new sqlite3.Database('./room-tracker.db')
 
 // Create tables
 db.serialize(() => {
@@ -37,7 +37,7 @@ db.serialize(() => {
         accessToken TEXT,
         refreshToken TEXT,
         roomCode INTEGER NOT NULL
-    )`);
+    )`)
 
   db.run(`CREATE TABLE IF NOT EXISTS rooms (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -45,179 +45,179 @@ db.serialize(() => {
         owner_id INTEGER NOT NULL UNIQUE, 
         FOREIGN KEY (owner_id)
           REFERENCES users (id)
-    )`);
-});
+    )`)
+})
 
 // Middleware to verify token
 const verifyToken = (req, res, next) => {
   // Extract token from headers or query parameters
-  const token = req.headers["authorization"] || req.query.token;
+  const token = req.headers.authorization || req.query.token
 
   if (!token) {
-    return res.status(403).send({ message: "No token provided." });
+    return res.status(403).send({ message: 'No token provided.' })
   }
 
   // Verify token
   jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(500).send({ message: "Failed to authenticate token." });
+      return res.status(500).send({ message: 'Failed to authenticate token.' })
     }
 
     // Save user information in request for use in other routes
-    req.user = decoded;
-    next();
-  });
-};
+    req.user = decoded
+    next()
+  })
+}
 
-app.get("/auth/callback", async (req, res) => {
-  const { code, state } = req.query;
-  const { tokens } = await oauth2Client.getToken(code);
-  oauth2Client.setCredentials(tokens);
+app.get('/auth/callback', async (req, res) => {
+  const { code } = req.query
+  const { tokens } = await oauth2Client.getToken(code)
+  oauth2Client.setCredentials(tokens)
 
   const oauth2 = google.oauth2({
     auth: oauth2Client,
-    version: "v2",
-  });
-  const { data } = await oauth2.userinfo.get(); // get user info
+    version: 'v2'
+  })
+  const { data } = await oauth2.userinfo.get() // get user info
 
   try {
     db.get(
       `SELECT * FROM users WHERE email = ?`,
-      [data["email"]],
+      [data.email],
       (err, row) => {
         if (err) {
-          res.status(201);
+          res.status(201)
         }
 
         if (row) {
           // Update user tokens if already exists
           db.run(
             `UPDATE users SET accessToken = ?, refreshToken = ?, roomCode = -1, WHERE email = ?`,
-            [tokens["access_token"], tokens["refresh_token"], data["email"]],
+            [tokens.access_token, tokens.refresh_token, data.email],
             function (err) {
               if (err) {
-                res.status(201);
+                res.status(201)
               }
 
-              res.status(200);
+              res.status(200)
             }
-          );
+          )
         } else {
           // Insert new user
           db.run(
             `INSERT INTO users (name, email, googleId, accessToken, refreshToken, roomCode) VALUES (?, ?, ?, ?, ?, ?)`,
             [
-              data["name"],
-              data["email"],
-              data["id"],
-              tokens["access_token"],
-              tokens["refresh_token"],
-              -1,
+              data.name,
+              data.email,
+              data.id,
+              tokens.access_token,
+              tokens.refresh_token,
+              -1
             ],
             function (err) {
               if (err) {
-                res.status(500);
+                res.status(500)
               }
 
-              res.status(201);
+              res.status(201)
             }
-          );
+          )
         }
       }
-    );
+    )
   } catch (error) {
-    console.error("Error verifying token:", error);
-    res.status(400);
+    console.error('Error verifying token:', error)
+    res.status(400)
   }
 
-  const token = jwt.sign(data["id"], process.env.TOKEN_SECRET, {
-    expiresIn: "4h",
-  });
+  const token = jwt.sign(data['id, process.env.TOKEN_SECRET, {
+    expiresIn: '4h'
+  })
 
   res.redirect(
-    `http://localhost:3000/room?token=${token}&googleId=${data["id"]}`
-  );
-});
+    `http://localhost:3000/room?token=${token}&googleId=${data.id}`
+  )
+})
 
-app.get("/auth", async (req, res) => {
+app.get('/auth', async (req, res) => {
   const scopes = [
-    "https://www.googleapis.com/auth/youtube.readonly",
-    "https://www.googleapis.com/auth/userinfo.email",
-    "https://www.googleapis.com/auth/userinfo.profile",
-  ];
+    'https://www.googleapis.com/auth/youtube.readonly',
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile'
+  ]
 
   const url = oauth2Client.generateAuthUrl({
-    access_type: "offline",
+    access_type: 'offline',
     scope: scopes,
-    state: req.query.user, // Pass user identifier in state
-  });
-  res.redirect(url);
-});
+    state: req.query.user // Pass user identifier in state
+  })
+  res.redirect(url)
+})
 
 // Function to generate a random 5-digit alphanumeric ID
 const generateCode = () => {
   const chars =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let code = "";
-  for (let i = 0; i < 5; i++) {
-    code += chars.charAt(Math.floor(Math.random() * chars.length));
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let code = ''
+  for (let i = 0 i < 5 i++) {
+    code += chars.charAt(Math.floor(Math.random() * chars.length))
   }
-  return code;
-};
+  return code
+}
 
 // Function to create a unique code
 const createUniqueCode = () => {
-  let code;
-  let rowExists = true;
+  let code
+  let rowExists = true
   do {
-    code = generateCode();
-    const sql = `SELECT 1 FROM rooms WHERE code = ? LIMIT 1`;
+    code = generateCode()
+    const sql = `SELECT 1 FROM rooms WHERE code = ? LIMIT 1`
     db.get(sql, [code], (err, row) => {
       if (err) {
-        return -1;
+        return -1
       }
 
       if (!row) {
-        rowExists = false;
+        rowExists = false
       } else {
-        rowExists = true;
+        rowExists = true
       }
-    });
-  } while (rowExists);
+    })
+  } while (rowExists)
 
   // Update table with code
-  const sql = `INSERT INTO rooms (code) VALUES (?)`;
+  const sql = `INSERT INTO rooms (code) VALUES (?)`
   db.run(sql, [code], function (err) {
     if (err) {
-      return -1;
+      return -1
     }
-  });
-  return code;
-};
+  })
+  return code
+}
 
 // Endpoint to create a new room
-app.post("/create-room", verifyToken, (req, res) => {
-  const { googleId } = req.query.googleId;
+app.post('/create-room', verifyToken, (req, res) => {
+  const { googleId } = req.query.googleId
 
-  let userId;
-  const sql = `SELECT 1 FROM users WHERE googleId = ? LIMIT 1`;
+  let userId
+  const sql = `SELECT 1 FROM users WHERE googleId = ? LIMIT 1`
   db.get(sql, [googleId], (err, row) => {
     if (err) {
-      return res.status(501).json({ message: `ERROR: not found user` });
+      return res.status(501).json({ message: `ERROR: not found user` })
     }
 
     if (!row) {
       return res
         .status(404)
-        .json({ message: `User with googleId ${googleId} not found` });
+        .json({ message: `User with googleId ${googleId} not found` })
     } else {
-      userId = row.id;
+      userId = row.id
     }
-  });
+  })
 
-  const newRoomCode = createUniqueCode();
-  if (newRoomCode == -1) {
-    res.status(501).json({ message: "ERROR: could not make room" });
+  const newRoomCode = createUniqueCode()
+  if (newRoomCode === -1) {
+    res.status(501).json({ message: 'ERROR: could not make room' })
   }
 
   db.run(
@@ -225,117 +225,118 @@ app.post("/create-room", verifyToken, (req, res) => {
     [userId, newRoomCode],
     function (err) {
       if (err) {
-        res.status(201);
+        res.status(201)
       }
     }
-  );
+  )
   db.run(
     `UPDATE users SET roomCode = ? WHERE id = ?`,
     [newRoomCode, userId],
     function (err) {
       if (err) {
-        res.status(201);
+        res.status(201)
       }
     }
-  );
+  )
 
-  return res.status(200).json({ codeId: newRoomCode });
-});
+  return res.status(200).json({ codeId: newRoomCode })
+})
 
 // Endpoint to join a room
-app.post("/join-room", verifyToken, (req, res) => {
-  const { googleId } = req.query.googleId;
-  const { roomCode } = req.query.roomCode;
+app.post('/join-room', verifyToken, (req, res) => {
+  const { googleId } = req.query.googleId
+  const { roomCode } = req.query.roomCode
 
-  let userId;
-  let sql = `SELECT 1 FROM users WHERE googleId = ? LIMIT 1`;
+  let userId
+  let sql = `SELECT 1 FROM users WHERE googleId = ? LIMIT 1`
   db.get(sql, [googleId], (err, row) => {
     if (err) {
-      return res.status(501).json({ message: `ERROR: not found user` });
+      return res.status(501).json({ message: `ERROR: not found user` })
     }
 
     if (!row) {
       return res
         .status(404)
-        .json({ message: `User with googleId ${googleId} not found` });
+        .json({ message: `User with googleId ${googleId} not found` })
     } else {
-      userId = row.id;
+      userId = row.id
     }
-  });
+  })
 
-  sql = `SELECT 1 FROM rooms WHERE roomCode = ? LIMIT 1`;
+  sql = `SELECT 1 FROM rooms WHERE roomCode = ? LIMIT 1`
   db.get(sql, [roomCode], (err, row) => {
     if (err) {
-      return res.status(501).json({ message: `ERROR: not found room` });
+      return res.status(501).json({ message: `ERROR: not found room` })
     }
 
     if (!row) {
       return res
         .status(404)
-        .json({ message: `room with roomCode ${roomCode} not found` });
+        .json({ message: `room with roomCode ${roomCode} not found` })
     }
-  });
+  })
 
   db.run(
     `UPDATE users SET roomCode = ? WHERE id = ?`,
     [roomCode, userId],
     function (err) {
       if (err) {
-        return res.status(501).json({ message: `ERROR: could not join room` });
+        return res.status(501).json({ message: `ERROR: could not join room` })
       }
     }
-  );
-  res.status(200).json({ message: "successfully joined room" });
-});
+  )
+  res.status(200).json({ message: 'successfully joined room' })
+})
 
-async function getSubscriptions(oauthToken) {
-  const auth = new google.auth.OAuth2();
-  auth.setCredentials({ access_token: oauthToken });
+async function getSubscriptions (oauthToken) {
+  const auth = new google.auth.OAuth2()
+  auth.setCredentials({ access_token: oauthToken })
 
   const response = await youtube.subscriptions.list({
     auth,
-    part: "snippet",
+    part: 'snippet',
     mine: true,
-    maxResults: 50,
-  });
+    maxResults: 50
+  })
 
-  return response.data.items.map((item) => item.snippet.customUrl); // Or return other relevant data
+  return response.data.items.map((item) => item.snippet.customUrl) // Or return other relevant data
 }
 
-app.get("/channels", (req, res) => {
-  const { roomCode } = req.query.roomCode;
+app.get('/channels', (req, res) => {
+  const { roomCode } = req.query.roomCode
 
-  const sql = "SELECT * FROM users WHERE roomCode = ?";
+  const sql = 'SELECT * FROM users WHERE roomCode = ?'
 
-  let allSubscriptions = [];
+  let allSubscriptions = []
   // Execute the query
   db.all(sql, [roomCode], (err, rows) => {
     if (err) {
-      throw err;
+      throw err
     }
 
+    let token
     // Iterate through the rows
     rows.forEach(async (row) => {
-      let token = row.accessToken;
+      token = row.accessToken
       try {
-        const subscriptions = await getSubscriptions(token);
-        allSubscriptions = [...allSubscriptions, ...subscriptions];
+        const subscriptions = await getSubscriptions(token)
+        allSubscriptions = [...allSubscriptions, ...subscriptions]
       } catch (error) {
         console.error(
           `Error fetching subscriptions for token ${token}:`,
           error
-        );
+        )
       }
-    });
-  });
+    })
+  })
 
   // Remove duplicates
-  allSubscriptions = [...new Set(allSubscriptions)];
+  allSubscriptions = [...new Set(allSubscriptions)]
 
-  res.json({ subscriptions: allSubscriptions });
-});
+  res.json({ subscriptions: allSubscriptions })
+})
 
 // Start server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
